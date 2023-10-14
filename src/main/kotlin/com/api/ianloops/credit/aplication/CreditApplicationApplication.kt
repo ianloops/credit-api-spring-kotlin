f0b91f2bd98b32a96205aab328231a7afa0eaa56
ianloops <ianllopesc@gmail.com>
2023-10-14 04:31:31 -0300

    spring starter

---
+++ src/main/kotlin/com/api/ianloops/credit/aplication/CreditApplicationApplication.kt
@@ -0,0 +1,11 @@
+package com.api.ianloops.credit.aplication
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class CreditApplicationApplication
+
+fun main(args: Array<String>) {
+	runApplication<CreditApplicationApplication>(*args)
+}
---
+++ src/test/kotlin/com/api/ianloops/credit/aplication/CreditApplicationApplicationTests.kt
@@ -0,0 +1,13 @@
+package com.api.ianloops.credit.aplication
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class CreditApplicationApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}
