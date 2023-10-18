95db6274b3d32cb9cf63d758d36d14cb63566805
ianloops <ianllopesc@gmail.com>
2023-10-17 23:21:02 -0300

    swagger-ui implemented

---
+++ src/main/kotlin/com/api/ianloops/credit/aplication/Configuration/Swagger3Config.kt
@@ -0,0 +1,14 @@
+package com.api.ianloops.credit.aplication.Configuration
+
+import org.springdoc.core.models.GroupedOpenApi
+import org.springframework.context.annotation.Bean
+import org.springframework.context.annotation.Configuration
+
+@Configuration
+class Swagger3Config {
+    @Bean
+    fun publicApi(): GroupedOpenApi? {
+        return GroupedOpenApi.builder().group("springcreditapplication-public")
+            .pathsToMatch("/customers/**", "/credits/**").build()
+    }
+}
